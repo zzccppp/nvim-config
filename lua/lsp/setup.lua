15c733c5c815872b13d4530ca38101801e34c8e3
ZZDirty <muztaga2@gmail.com>
2022-02-05 18:50:23 +0800

    add julails.lua

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -6,7 +6,8 @@ local lsp_installer = require "nvim-lsp-installer"
 local servers = {
   sumneko_lua = require "lsp.lua", -- /lua/lsp/lua.lua
   clangd = require "lsp.clangd",
-  pylsp = require "lsp.pylsp"
+  pylsp = require "lsp.pylsp",
+  julials = require "lsp.pylsp"
 }
 
 -- 自动安装 LanguageServers
