f99dfcb25283fba0a9b7e6205d3acd976b086503
ZZDirty <muztaga2@gmail.com>
2022-03-25 14:46:40 +0800

    add some telescope config and packer auto download

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,9 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -40,5 +46,10 @@ return require('packer').startup(function()
   use 'simrat39/rust-tools.nvim'
   use("glepnir/dashboard-nvim")
   use("ahmedkhalf/project.nvim")
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
