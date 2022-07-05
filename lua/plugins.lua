d9f47edb8e84e0737b51f63f2635441f02e86c3a
ZZDirty <muztaga2@gmail.com>
2022-07-05 12:45:19 +0800

    add some files
    交互式变基操作正在进行中；至 624cd49
    最后完成的命令（1 条命令被执行）：
       pick e32d3aa add some files
    接下来要执行的命令（剩余 2 条命令）：
       pick 3326640 add ignore files
       pick 754d92c remove plugin files
    您在执行将分支 'master' 变基到 '624cd49' 的操作。

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,7 @@ return require('packer').startup(function()
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons'
   }
-  use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
+  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use {'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer'}
   -- nvim-cmp
@@ -48,12 +48,12 @@ return require('packer').startup(function()
   use("ahmedkhalf/project.nvim")
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use {
-  "folke/todo-comments.nvim",
-  requires = "nvim-lua/plenary.nvim",
-  config = function()
-    require("todo-comments").setup {
-    }
-  end
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+      }
+    end
   }
   use("lukas-reineke/indent-blankline.nvim")
 
